c7d29aea3ded0d93b9d6a72a6f53a3dd569f9bcd
yudedako <pluto.club+git@gmail.com>
2022-05-10 07:45:58 +0900

    最小のページ遷移を実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,43 @@
 import 'package:flutter/material.dart';
-import 'pages/HomePage.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(MaterialApp(
+    title: 'Navigation',
+    home: FirstScreen(),
+  ));
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('1番目のルート'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            child: Text('次の画面を開く'),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SecondScreen()));
+            },
+          ),
+        ),
+      );
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('2番目のルート'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            child: Text('戻る'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      );
 }
