99c24b971c7d7bec4f9243cb55af8fe5850e0c02
yudedako <pluto.club+git@gmail.com>
2022-05-24 08:52:28 +0900

    APIアクセス用のクラスを作成

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_note/main.dart';
+import 'package:pokemon_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
